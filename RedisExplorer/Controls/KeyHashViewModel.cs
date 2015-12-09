e9e676f928e9f7288faa0297cc697fdfa59a2c3c
Lee <gould.lee@googlemail.com>
2015-12-09 10:46:17 +0000

    hashes

---
+++ RedisExplorer/Controls/KeyHashViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Caliburn.Micro;
+
+using RedisExplorer.Interface;
+using RedisExplorer.Messages;
+using RedisExplorer.Models;
+
+namespace RedisExplorer.Controls
+{
+    public class KeyHashViewModel: Screen, IHandle<TreeItemSelectedMessage>, IHandle<AddKeyMessage>, IValueItem
+    {
+        private BindableCollection<KeyValuePair<string, string>> keyValuesDict;
+
+        public BindableCollection<KeyValuePair<string, string>> KeyValuesDict
+        {
+            get
+            {
+                return keyValuesDict;
+            }
+            set
+            {
+                keyValuesDict = value ?? new BindableCollection<KeyValuePair<string, string>>();
+                NotifyOfPropertyChange(() => KeyValuesDict);
+            }
+        }
+
+        public KeyHashViewModel(IEventAggregator eventAggregator)
+        {
+            var eAggregator = eventAggregator;
+            eAggregator.Subscribe(this);
+        }
+
+        public void Handle(TreeItemSelectedMessage message)
+        {
+            if (message != null && message.SelectedItem is RedisKeyList && !message.SelectedItem.HasChildren)
+            {
+                DisplayValue((RedisKeyHash)message.SelectedItem);
+            }
+        }
+
+        private void DisplayValue(RedisKeyHash item)
+        {
+            if (item != null)
+            {
+                var value = item.KeyValues;
+
+                KeyValuesDict = new BindableCollection<KeyValuePair<string, string>>(value.Select(x => new KeyValuePair<string, string>(x.Key, x.Value)));
+            }
+        }
+
+        public void Handle(AddKeyMessage message)
+        {
+            KeyValuesDict = new BindableCollection<KeyValuePair<string, string>>();
+        }
+    }
+}
---
+++ RedisExplorer/Models/RedisKeyHash.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Caliburn.Micro;
+
+using StackExchange.Redis;
+
+namespace RedisExplorer.Models
+{
+    public class RedisKeyHash : RedisKey
+    {
+        private Dictionary<string, string> keyValues;
+
+        public RedisKeyHash(TreeViewItem parent, IEventAggregator eventAggregator) : base(parent, eventAggregator)
+        {
+        }
+
+        public Dictionary<string, string> KeyValues
+        {
+            get
+            {
+                if (keyValues != null)
+                {
+                    return keyValues;
+                }
+
+                var key = KeyName;
+                var db = Database;
+                if (db.KeyExists(key))
+                {
+                    keyValues = db.HashGetAll(key).ToStringDictionary();
+                }
+                else
+                {
+                    keyValues = new Dictionary<string, string>();
+                }
+
+                return keyValues;
+            }
+            set
+            {
+                keyValues = value;
+            }
+        }
+
+        public override bool Save()
+        {
+            var existingkey = Database.KeyExists(KeyName);
+            var saved = false;
+
+            if (KeyType == RedisType.Hash)
+            {
+                // TODO
+            }
+
+            return saved;
+        }
+
+        public override void Reload()
+        {
+            KeyValues = null;
+            base.Reload();
+        }
+    }
+}
