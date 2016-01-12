11c2357e54a3cb725a1964ddc896b50b2059a271
Lee <gould.lee@googlemail.com>
2016-01-12 09:59:38 +0000

    default view

--- RedisExplorer/Controls/DefaultViewModel.cs
+++ RedisExplorer/Controls/DefaultViewModel.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+
 using Caliburn.Micro;
+
 using RedisExplorer.Interface;
 
 namespace RedisExplorer.Controls
 {
     public class DefaultViewModel : Screen, IValueItem
     {
-        public DefaultViewModel()
+        public void NavigateTo(string url)
         {
+            Process.Start(new ProcessStartInfo(url));
         }
     }
 }
